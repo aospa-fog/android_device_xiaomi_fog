205ca4732c9f7a3a40412e4bbe12c9bcf1c504e9
alternoegraha <noegrahachan@gmail.com>
2025-03-26 15:39:32 +0700

    fog: Drop LMKD props
    - This may break compatibility with kernel that doesn't use kernel level LMK (such as simple_lmk)
    - and since ro.vendor.lmk.force_inkernel_lmk is set to true, LMKD props will not really used I think so it's useless
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -257,20 +257,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
-# LMKD
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.lmk.log_stats=false \
-    ro.lmk.kill_heaviest_task=false \
-    ro.lmk.kill_timeout_ms=100 \
-    ro.lmk.use_minfree_levels=true \
-    ro.lmk.critical_upgrade=true \
-    ro.lmk.upgrade_pressure=40 \
-    ro.lmk.downgrade_pressure=60 \
-    ro.lmk.filecache_min_kb=153600 \
-    ro.lmk.thrashing_limit=30 \
-    ro.lmk.thrashing_limit_decay=50 \
-    ro.lmk.stall_limit_critical=40
-
 # LMK
 PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.lmk.force_inkernel_lmk=true
